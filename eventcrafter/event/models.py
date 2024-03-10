3411c667cf4463725462296b4a7b0004f2f8a621
yunus <yunusolcar@hotmail.com>
2024-03-10 11:25:09 +0300

    django form issue

--- eventcrafter/event/models.py
+++ eventcrafter/event/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.admin.widgets import AdminDateWidget
+from django import forms
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    is_interested = models.BooleanField(default=False)
+    member = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    event_date = forms.DateTimeField(widget=AdminDateWidget())
+
+    def __str__(self):
+        return self.name
+
+
