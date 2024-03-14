cde2b44e4578fdaeacc1fdfc6398f05a00706ecf
yunus <yunusolcar@hotmail.com>
2024-03-14 22:39:52 +0300

    feat: ui completely changed

--- eventcrafter/event/views.py
+++ eventcrafter/event/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from event.models import Event
-from .forms import EventForm
+
+
 # Create your views here.
 def index(request):
     context = {'events': Event.objects.all()}
-    return render(request,'event/index.html', context)
+    return render(request, 'event/index.html', context)
+
 
 def events(request):
     context = {'events': Event.objects.all()}
     return render(request, 'event/events.html', context)
 
+
 def single_event(request, slug):
     event = Event.objects.get(slug=slug)
-    return render(request, 'event/single-event.html', {"event":event})
-
-def create_event(request):
-    if request.method == 'POST':
-        form
+    return render(request, 'event/single-event.html', {"event": event})
