4129c80d3c433f5304ef95c3b843e494364ef101
yunus <yunusolcar@hotmail.com>
2024-03-17 11:28:36 +0300

    partials, ui improvements

--- eventcrafter/event/urls.py
+++ eventcrafter/event/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("events", views.events, name='events'),
-    path("events/<slug:slug>", views.single_event, name='single-event')
+    path("events/<slug:slug>", views.single_event, name='single-event'),
+    path("about", views.about, name='about'),
+    path("create-event", views.create_event, name='create-event')
 ]
--- eventcrafter/event/views.py
+++ eventcrafter/event/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from event.models import Event
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def index(request):
-    context = {'events': Event.objects.all()}
+    events = Event.objects.all()
+    participant_count = User.objects.all().count()
+    event_count = Event.objects.all().count()
+    context = {
+        'events': events,
+        'participant_count': participant_count,
+        'event_count': event_count
+    }
+
     return render(request, 'event/index.html', context)
 
 
@@ -16,3 +25,11 @@ def events(request):
 def single_event(request, slug):
     event = Event.objects.get(slug=slug)
     return render(request, 'event/single-event.html', {"event": event})
+
+
+def about(request):
+    return render(request, 'event/about.html')
+
+
+def create_event(request):
+    return render(request, 'event/create-event.html')
