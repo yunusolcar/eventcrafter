babbea42c70b7357ccf5fe3c2ed2759a865e5c88
yunus <yunusolcar@hotmail.com>
2024-04-23 18:33:13 +0300

    feat: ui improvements

--- eventcrafter/event/forms.py
+++ eventcrafter/event/forms.py
@@ -12,7 +12,7 @@ class CreateEventForm(forms.ModelForm):
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'location': widgets.TextInput(attrs={'class': 'form-control'}),
-            'image': widgets.FileInput(),
+            'image': widgets.FileInput(attrs={'class': 'form-control'}),
             'event_date': widgets.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'creator': widgets.HiddenInput(),
         }
